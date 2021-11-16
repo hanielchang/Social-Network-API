080bc633c747161e0f831580c7eb4c41d738f9fa
hanielchang <haniellloyd@gmail.com>
2021-11-15 23:01:11 -0800

    removed unrelated files

--- models/User.js
+++ models/User.js
@@ -44,9 +44,3 @@ UserSchema.virtual('friendCount').get(function () {
 // They're normally computed values that get evaluated when you try to access their properties.
 // Virtuals allow us to add more information to a database response so that we don't have 
 // to add in the information manually with a helper before responding to the API request.
-
-// ----------------line 43----------------
-// In its basic form, .reduce() takes two parameters, an accumulator and a currentValue. 
-// Here, the accumulator is total, and the currentValue is comment. As .reduce() walks through 
-// the array, it passes the accumulating total and the current value of comment into the function, 
-// with the return of the function revising the total for the next iteration through the array.
